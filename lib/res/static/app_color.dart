c6dde56090caedd6f5be76a0010c6dd90af84c08
vijay vekariya <vijayvekariya007@gmail.com>
2024-04-05 16:17:56 +0530

    5-4-24

--- lib/res/static/app_color.dart
+++ lib/res/static/app_color.dart
@@ -4,4 +4,5 @@ class AppColor {
   static Color blackcolor = Colors.black;
   static Color whitecolor = Colors.white;
   static Color lightblackcolor = Color(0xff909090);
+  static Color extralightblack = Color.fromARGB(255, 234, 232, 232);
 }
